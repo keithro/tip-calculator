588e0045d962fd976c7fc6c7da8493333f86057f
Keith Rodriguez <keithrodriguez@rightpoint.com>
2022-07-27 18:19:53 -0400

    Moved calculations to selectors

--- src/containers/SummaryContainer.js
+++ src/containers/SummaryContainer.js
@@ -1,21 +1,25 @@
 import { connect } from 'react-redux';
 import { Summary } from '../components/Summary';
+import {
+  selectSubtotal,
+  selectTipAmount,
+  selectTotal
+} from '../store/items/selector';
 
 const mapStateToProps = (state) => {
-  // const items = state.items;
-  // let subtotal = 0;
-  // for(const item of items) {
-  //   subtotal += item.quantity * item.price;
-  // }
+  // const subtotal = state.items.reduce(
+  //   (sum, item) => (sum += item.price * item.quantity),
+  //   0
+  // );
 
-  const subtotal = state.items.reduce(
-    (sum, item) => (sum += item.price * item.quantity),
-    0
-  );
+  // const tipAmount = subtotal * (state.tipPercentage / 100);
 
-  const tipAmount = subtotal * (state.tipPercentage / 100);
+  // const total = subtotal + tipAmount;
 
-  const total = subtotal + tipAmount;
+  // With our new selectors (imported from selector.js) we can get the amounts from our selector functions
+  const subtotal = selectSubtotal(state);
+  const tipAmount = selectTipAmount(state);
+  const total = selectTotal(state);
 
   return {
     subtotal,
