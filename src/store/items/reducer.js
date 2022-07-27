a866335347be0d51b3a4ce03e8df9088f543926d
Keith Rodriguez <keithrodriguez@rightpoint.com>
2022-07-27 10:04:06 -0400

    mapStateToProps and connect API

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,11 +1,20 @@
+import { ITEM_ADDED } from './actions';
+
 let id = 1;
 
 export const initialItems = [
-  { uuid: id++, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
+  { uuid: id++, name: 'Awesome Tofu Roast', price: 14, quantity: 1 },
+  { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
 ];
 
 export const reducer = (state = initialItems, action) => {
+  if (action.type === ITEM_ADDED) {
+    // Spread the rest of the items incase we add or override something
+    const item = { uuid: id++, quantity: 1, ...action.payload };
+    // Returning an array with all other items + the new item we created
+    return [...state, item];
+  }
+
   return state;
 };
 
